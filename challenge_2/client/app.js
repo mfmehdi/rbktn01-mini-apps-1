eae82fed59a7e4244c875e533735dd4ea69c4837
mfmehdi <mfmehdi2@gmail.com>
2019-12-30 10:00:13 +0100

    with ajax

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,15 +1,59 @@
-// $("#form").submit(function(e) {
-//   e.preventDefault(); // avoid to execute the actual submit of the form.
+$("#form").submit(function(e) {
+  e.preventDefault(); // avoid to execute the actual submit of the form.
 
-//   var form = $(this);
-//   var url = form.attr("action");
+  var form = $(this);
+  var url = form.attr("action");
+  console.log(url);
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: form.serialize(), // serializes the form's elements.
+    crossDomain: true,
+    xhrFields: {
+      responseType: "blob"
+    },
+    success: function(response, status, xhr) {
+      var a = document.createElement("a");
+      console.log("a===>", a);
+      console.log(response);
+      var url = window.URL.createObjectURL(response);
+      //  console.log("url===>", url);
+      a.href = url;
+      a.download = "my.csv";
+      $(a).html("download");
+      $("#link").append(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+      // console.log(xhr); // show response from the php script.
+    }
+  });
+});
 
-//   $.ajax({
-//     type: "POST",
-//     url: url,
-//     data: form.serialize(), // serializes the form's elements.
-//     success: function(data) {
-//       console.log(data); // show response from the php script.
-//     }
-//   });
+// $.fileDownload({ type: 'POST', url: 'url', data: form_data, processData: false, contentType: false, success: function() { // $.unblockUI(); } });
+// $(document).ready(function(){
+//     $("#submit").bind('click',function(){
+//         var idiom=$("#idiom").val();
+//         $.ajax({
+//             type: "GET",
+//             url: 'http://www.oxfordlearnersdictionaries.com/search/english/direct/',
+//             data:{q:idiom},
+//             async:true,
+//             crossDomain:true,
+//             success: function(data, status, xhr) {
+//                 alert(xhr.getResponseHeader('Location'));
+//             }
+//         });
+
+//     });
 // });
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       // document.getElementById("demo").innerHTML = "fff";
+//       console.log("ok");
+//     }
+//   };
+//   //xhttp.open("GET", "ajax_info.txt", true);
+//   xhttp.send();
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,29 +1,53 @@
 const express = require("express");
 const path = require("path");
+// var cors = require("cors");
 const app = express();
+
 const bodyParser = require("body-parser");
 const fs = require("fs");
 var csv = require("fast-csv");
 
+// app.use(cors());
 app.use(express.static("client"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var ws = fs.createWriteStream("my.csv");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}); // cros  problem
+
 const port = 4000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+var count = 0;
 
 app.post("/upload", (req, res) => {
   var texJson = req.body.textJson;
 
-  covertJsonToCsv(texJson, datas => {
-    res.download(path.join(__dirname, "my.csv"));
+  covertJsonToCsv(texJson, (ws, datas) => {
+    // ws.on("finish", () => {
+    //   console.log("finish");
+    // });
+    //   ws.end();
+    console.log("my" + count + ".csv");
+    var p = path.join(__dirname, "my" + count + ".csv");
+
+    csv.write(datas, { headers: true }).pipe(ws);
+    //fs.writeFileSync("my.csv", datas);
+
+    res.download(p);
+    count++;
   });
 
   //   res.end();
 });
 
 var covertJsonToCsv = function(text, callback) {
+  var ws = fs.createWriteStream("my" + count + ".csv");
   var myObj = JSON.parse(text);
   var keys = Object.keys(myObj);
   keys.pop();
@@ -31,8 +55,13 @@ var covertJsonToCsv = function(text, callback) {
   // console.log(datas);
   datas.unshift(keys);
   //  var data = Object.values(myObj);
-  csv.write(datas, { headers: true }).pipe(ws);
-  callback(datas);
+  // csv
+  //   .write(datas, { headers: true })
+  //   .pipe(ws)
+  //   .then(datas => {
+  //     callback(datas);
+  //   });
+  callback(ws, datas);
 };
 
 var getData = function(obj, datas) {
