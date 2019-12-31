2d1b4c84743cfd1769f3cba3eac9a9e21becc8af
mfmehdi <mfmehdi2@gmail.com>
2019-12-31 11:13:06 +0100

    webpack

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,9 @@
+const express = require("express");
 
+const app = express();
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const port = 4000;
 
+app.listen(port, () => console.log(`app listening on port ${port}!`));
+app.use(cors());
---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/client/dist"
+  }
+};
